b333ba8151564818b20b7088fbf8be2f46f2b348
Prasad Peddada <ppeddada@salesforce.com>
2023-11-07 14:54:15 -0800

    Minor clean up

--- src/main/java/com/salesforce/security/tks/TrustStoreProvider.java
+++ src/main/java/com/salesforce/security/tks/TrustStoreProvider.java
@@ -1,6 +1,8 @@
 package com.salesforce.security.tks;
 
 import java.security.Provider;
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * @author ppeddada
@@ -10,10 +12,28 @@ public final class TrustStoreProvider extends Provider {
 
     private static final long serialVersionUID = 1L;
     public static final String NAME = "SalesforceTrustStoreProvider";
+    private static final String TYPE = "KeyStore";
+    private final Service service;
+    private final Set<Service> services;
 
     public TrustStoreProvider() {
-        super(NAME, "1.1", "A PEM file backed trust store provider");
-        put("KeyStore." + ReadOnlyPEMTrustStore.NAME, ReadOnlyPEMTrustStore.class.getName());
+        super(NAME, "1.3", "A PEM file backed trust store provider");
+        this.service = new Service(this, TYPE, ReadOnlyPEMTrustStore.NAME, ReadOnlyPEMTrustStore.class.getName(),
+                Collections.emptyList(), Collections.emptyMap());
+        services = Set.of(service);
+    }
+
+    @Override
+    public Service getService(String type, String algorithm) {
+        if (ReadOnlyPEMTrustStore.NAME.equals(algorithm) && TYPE.equals(type)) {
+            return this.service;
+        }
+        return null;
+    }
+
+    @Override
+    public Set<Service> getServices() {
+        return services;
     }
 
 }
